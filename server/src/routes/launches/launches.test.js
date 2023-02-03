7471d501a1377df1974d14f3f386c3a0d63f8848
RobJ <robjjohnsenwork@gmail.com>
2023-02-03 13:26:23 +0700

    Added tests for missing props, and invalid dates

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../../app");
 //
-// Lets write some API tests! -> Use SuperTest library
+// Lets write some API tests! -> Use SuperTest library - alternating between the jest functionality and the superTest built on func
 //
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
@@ -16,20 +16,65 @@ describe("Test GET /launches", () => {
 });
 //
 describe("Test POST /launch", () => {
+  // Code sitting here can be resused in the diff tests..
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-199 f",
+    launchDate: "January 4, 2028",
+  };
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-199 f",
+  };
+  const launchDateWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-199 f",
+    launchDate: "incorrect date",
+  };
+  //
   test("It should respond with 201 success", async () => {
-    const res = await request(app)
+    const response = await request(app)
       .post("/launches")
-      .send({
-        mission: "USS Enterprise",
-        rocket: "NCC 1701-D",
-        target: "Kepler-199 f",
-        launchDate: "January 4, 2028",
-      })
+      .send(completeLaunchData)
       .expect("Content-Type", /json/)
       .expect(201);
+    // valueOf() to get the matching nums?
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(requestDate).toBe(responseDate);
+    // toMatchObject matching loosely
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  //
+  //
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    // toStrictEqual is for strcit matching,, type and structures
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
   });
   //
-  test("It should catch missing required properties", () => {});
   //
-  test("It should catch invalid dates", () => {});
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDateWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    // toStrictEqual is for strcit matching,, type and structures
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
 });
